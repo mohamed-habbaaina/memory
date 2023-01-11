70b45c84428cedcdd226a23665bc44214948c6d1
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-01-11 10:03:19 +0100

    creation de la class cards, __construct: shuffle les cartes et stokage dans ne variable de session

--- class/Cards.php
+++ class/Cards.php
@@ -1,42 +1,21 @@
 <?php
-class Cards
-{
-    public $name;
-    private $depl;
-    public $valu;
-    private $tab_card_value = ['a.png', 'b.png', 'c.png', 'a.png', 'b.png', 'c.png'];
-    // private $tab_card = ['a', 'b', 'c', 'd', 'e', 'f'];
+class cards{
+    private $tab_cards = ['a.jpeg', 'b.jpeg', 'c.jpeg', 'a.jpeg', 'b.jpeg', 'c.jpeg', 'd.jpeg', 'e.jpeg', 'f.jpeg', 'd.jpeg', 'e.jpeg', 'f.jpeg', 'g.jpeg', 'h.jpeg', 'i.jpeg', 'g.jpeg', 'h.jpeg', 'i.jpeg'];
+    private $tab_cards_level = ['a.jpeg', 'b.jpeg', 'c.jpeg', 'a.jpeg', 'b.jpeg', 'c.jpeg'];
+
+    // stokage du tableau des cards dans une variable session, avec shuffle pour melanger les cards.
     public function __construct()
     {
-        shuffle($this->tab_card_value);
-    }
-    public function deply(){
-        // $this->depl = $depl;
-        return true;
-    }
-    // public function image($x){
-    //     return $x.'.png';
-    // }
-    // public function valu(){
-    //     return $this->valu;
-    // }
-    public function affich(){
-        return
-            '<table>
-    <tbody>
-        <tr>
-        <td><img src="../img/' . $this->tab_card_value[0] . '"></td>
-        <td><img src="../img/' . $this->tab_card_value[1] . '"></td>
-        <td><img src="../img/' . $this->tab_card_value[2] . '"></td>
-        </tr>
-        <tr>
-        <td><img src="../img/' . $this->tab_card_value[3] . '"></td>
-        <td><img src="../img/' . $this->tab_card_value[4] . '"></td>
-        <td><img src="../img/' . $this->tab_card_value[5] . '"></td>
-        </tr>
-    </tbody>
-</table>';
+        if (!isset($_SESSION['tab_cards_level'])):
+            shuffle($this->tab_cards_level);
+            $_SESSION['tab_cards_level'] = $this->tab_cards_level;
+        else:
+            $this->tab_cards_level = $_SESSION['tab_cards_level'];
+        endif;
+        var_dump($_SESSION['tab_cards_level']);
     }
 
-// shuffle()
+
+
+
 }
